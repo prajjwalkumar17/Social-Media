e82e6dbad4741d5477f50b2e6e5df397c68b63ea
PrAjJwAl KuMaR <talk2prajjwal@gmail.com>
2021-04-18 09:58:41 +0530

    done the design and splash screen

--- app/src/main/java/com/pk/socialmedia/MainActivity.java
+++ app/src/main/java/com/pk/socialmedia/MainActivity.java
@@ -2,13 +2,24 @@ package com.pk.socialmedia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(),signup.class));
+            }
+        },3000);
+
     }
 }
